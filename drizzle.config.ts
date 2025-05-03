5000a4feae1d2dc8763f9a27f0448c3939937fb7
Kirill Mokevnin <mokevnin@gmail.com>
2025-05-03 12:47:26 -0400

    fix drizzle config
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 import { dbUrl } from './config.js'
 export default defineConfig({
   out: './drizzle',
-  schema: './src/db/schema.ts',
+  schema: './lib/db/schema.ts',
   dialect: 'sqlite',
   dbCredentials: {
     url: dbUrl,
